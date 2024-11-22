745d78217769111784ccfc5b5d3665ba0735564d
JunnieYAHH <gerelitopuyos02@gmail.com>
2024-11-22 18:31:44 +0800

    Update now

---
+++ my-app/assets/common/baseUrl.js
@@ -0,0 +1,16 @@
+import { Platform } from 'react-native'
+
+
+
+let baseURL = '';
+
+{Platform.OS == 'android'
+
+// ? baseURL = 'http://192.168.55.101:8080/api/v1'
+// : baseURL = 'http://192.168.55.101:8080/api/v1'
+
+? baseURL = 'https://pspmobile.onrender.com/api/v1'
+: baseURL = 'https://pspmobile.onrender.com/api/v1'
+}
+
+export default baseURL;
